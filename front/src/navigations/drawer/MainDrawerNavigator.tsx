674a0615b839a1f4ccb866ab8fb58058cac12ca2
crown0205 <crown0205@gmail.com>
2024-08-14 16:44:14 +0900

    add : calendar, feed screen 추가

--- front/src/navigations/drawer/MainDrawerNavigator.tsx
+++ front/src/navigations/drawer/MainDrawerNavigator.tsx
@@ -2,6 +2,8 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import React from 'react';
 import {View, Text} from 'react-native';
 import MapHomeScreen from '../../screens/MapHomeScreen';
+import FeedHomeScreen from '../../screens/FeedHomeScreen';
+import CalendarHomeScreen from '../../screens/CalendarHomeScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -9,6 +11,8 @@ const MainDrawerNavigator = () => {
   return (
     <Drawer.Navigator>
       <Drawer.Screen name="MapHome" component={MapHomeScreen} />
+      <Drawer.Screen name="FeedHome" component={FeedHomeScreen} />
+      <Drawer.Screen name="CalendarHome" component={CalendarHomeScreen} />
     </Drawer.Navigator>
   );
 };
---
+++ front/src/screens/CalendarHomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const CalendarHomeScreen = () => {
+  return (
+    <View>
+      <Text>CalendarHomeScreen</Text>
+    </View>
+  );
+};
+
+export default CalendarHomeScreen;
