ab73753c4499b07d0b492419cb622366369a8a2b
crown0205 <crown0205@gmail.com>
2024-08-14 17:34:20 +0900

    add : button 공통 컴포넌트 추가 * colors 상수로 뺌

--- front/src/screens/auth/AuthHomeScreen.tsx
+++ front/src/screens/auth/AuthHomeScreen.tsx
@@ -1,8 +1,9 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {Button, SafeAreaView, View} from 'react-native';
-import {authNavigations} from '../../constants';
+import {authNavigations} from '../../constants/navigations';
 import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
+import CustomButton from '../../components/CustomButton';
 
 type AuthHomeScreenProps = StackScreenProps<
   AuthStackParamList,
@@ -13,12 +14,13 @@ const AuthHomeScreen = ({navigation}: AuthHomeScreenProps) => {
   return (
     <SafeAreaView>
       <View>
-        <Button
-          title="로그인화면으로 이동"
+        <CustomButton
+          label="로그인화면으로 이동"
           onPress={() => navigation.navigate(authNavigations.LOGIN)}
         />
-        <Button
-          title="회원가입으로 이동"
+        <CustomButton
+          label="회원가입으로 이동"
+          variant="outlined"
           onPress={() => navigation.navigate(authNavigations.SIGNUP)}
         />
       </View>
