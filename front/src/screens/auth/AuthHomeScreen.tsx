2bd4b2b73b89ea8f5422d9e1c2aeea8e17f78a46
crown0205 <crown0205@gmail.com>
2024-08-19 15:44:18 +0900

    add : 로그인 화면 디자인 변경

--- front/src/screens/auth/AuthHomeScreen.tsx
+++ front/src/screens/auth/AuthHomeScreen.tsx
@@ -1,6 +1,13 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Button, SafeAreaView, View} from 'react-native';
+import {
+  Button,
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from 'react-native';
 import {authNavigations} from '../../constants/navigations';
 import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
 import CustomButton from '../../components/CustomButton';
@@ -12,8 +19,15 @@ type AuthHomeScreenProps = StackScreenProps<
 
 const AuthHomeScreen = ({navigation}: AuthHomeScreenProps) => {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image
+          resizeMode="contain"
+          style={styles.logo}
+          source={require('../../assets/foodiLog.png')}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
         <CustomButton
           label="로그인화면으로 이동"
           onPress={() => navigation.navigate(authNavigations.LOGIN)}
@@ -29,3 +43,27 @@ const AuthHomeScreen = ({navigation}: AuthHomeScreenProps) => {
 };
 
 export default AuthHomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 20,
+  },
+  logoContainer: {
+    flex: 1,
+    width: Dimensions.get('screen').width / 2,
+  },
+  logo: {
+    width: '100%',
+    height: '100%',
+  },
+  buttonContainer: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    gap: 10,
+    marginBottom: 20,
+  },
+});
