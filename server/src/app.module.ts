f825ec2db96520fd0a0dd15aae7a38237966ed99
crown0205 <crown0205@gmail.com>
2024-08-21 12:32:34 +0900

    chore : connect db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'hyeonsujeong',
+      password: 'postgres',
+      database: 'foodielog-app',
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
+  ],
   providers: [],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3030);
+  console.log(`http://localhost:${3030}`);
 }
 bootstrap();
