ec633ff0016e3040c40abc9fb1cd50c5b926cc58
crown0205 <crown0205@gmail.com>
2024-08-26 14:02:56 +0900

    feat : add signup api * add user entity * add auth.dto * install bcrypt

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
+import { AuthDto } from './dto/auth.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async signup(authDto: AuthDto) {
+    const { email, password } = authDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.userRepository.create({
+      email,
+      password: hashedPassword,
+      loginType: 'email',
+    });
+
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      console.log(error);
+      if (error.code === '23505') {
+        throw new ConflictException('이미 존재하는 이메일입니다.');
+      }
+
+      throw new InternalServerErrorException(
+        '회원가입 도중 에거가 발생했습니다.',
+      );
+    }
+  }
+}
