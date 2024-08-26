feea86e8f29fab430046944ea6ea2ddf1e90daeb
crown0205 <crown0205@gmail.com>
2024-08-26 13:39:46 +0900

    feat : 인증 모듈 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostController } from './post/post.controller';
 import { PostModule } from './post/post.module';
 import { PostService } from './post/post.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { PostService } from './post/post.service';
       synchronize: true,
     }),
     PostModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
