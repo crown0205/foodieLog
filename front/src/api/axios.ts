e610ab510c2fdf8b1c88118467298aad7a0ac7a4
crown0205 <crown0205@gmail.com>
2024-09-15 00:52:05 +0900

    fix : axios.ts 파일에 안드로이드에서 localhost가 동작하지 않는 문제를 해결하기 위해 baseURL을 수정

--- front/src/api/axios.ts
+++ front/src/api/axios.ts
@@ -1,7 +1,10 @@
+import {deviceType} from '@/utils';
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3030',
+  // NOTE : 안드로이드에서는 localhost가 잘 동작하지 않으므로, 10.0.2.2를 사용
+  baseURL:
+    deviceType === 'android' ? 'http://10.0.2.2:3030' : 'http://localhost:3030',
   withCredentials: true, // NOTE : 쿠키를 주고받기 위해 필요
 });
 
