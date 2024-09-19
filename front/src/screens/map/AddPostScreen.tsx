e571f44dcb330853cc2bae1c951f6fca84b159b9
crown0205 <crown0205@gmail.com>
2024-09-19 23:03:11 +0900

    feat : get addPostScreen route params

--- front/src/screens/map/AddPostScreen.tsx
+++ front/src/screens/map/AddPostScreen.tsx
@@ -1,10 +1,18 @@
+import { MapStackParamList } from '@/navigations/stack/MapStackNavigator';
+import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const AddPostScreen = () => {
+type AddPostScreenProps = StackScreenProps<MapStackParamList, 'AddPost'>;
+
+const AddPostScreen = ({ route }: AddPostScreenProps) => {
+  const { location } = route.params;
   return (
     <View>
       <Text>AddPostScreen</Text>
+
+      <Text>{location.latitude}</Text>
+      <Text>{location.longitude}</Text>
     </View>
   );
 };
