4ce821d94009e3ffbf14ab5c3c58b616b71a66c6
crown0205 <crown0205@gmail.com>
2024-09-22 22:11:39 +0900

    refactor : existing marker data and add new marker data (to reduce unnecessary API requests)

--- front/src/hooks/queries/useMutateCreatePost.ts
+++ front/src/hooks/queries/useMutateCreatePost.ts
@@ -2,15 +2,32 @@ import { createPost } from '@/api';
 import queryClient from '@/api/queryClient';
 import { queryKeys } from '@/constants';
 import { UseMutationCustomOptions } from '@/types/common';
+import { Marker } from '@/types/domain';
 import { useMutation } from '@tanstack/react-query';
 
 function useMutateCreatePost(mutationOptions?: UseMutationCustomOptions) {
   return useMutation({
     mutationFn: createPost,
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
-      });
+    onSuccess: newPost => {
+      // queryClient.invalidateQueries({
+      //   queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
+      // });
+
+      // NOTE : 기존의 marker 데이터를 가져와서 새로운 marker 데이터를 추가 ( 불필요한 API 요청을 줄이기 위함 )
+      queryClient.setQueryData<Marker[]>(
+        [queryKeys.MARKER, queryKeys.GET_MARKERS],
+        prevMarkers => {
+          const newMarker: Marker & { score: number } = {
+            id: newPost.id,
+            latitude: newPost.latitude,
+            longitude: newPost.longitude,
+            color: newPost.color,
+            score: newPost.score,
+          };
+
+          return prevMarkers ? [...prevMarkers, newMarker] : [newMarker];
+        },
+      );
     },
     ...mutationOptions,
   });
