f091f67d63b2dcf2e07bd6d07ee41313c34e396f
crown0205 <crown0205@gmail.com>
2024-10-20 14:38:38 +0900

    feat : add custom toast style

--- front/App.tsx
+++ front/App.tsx
@@ -1,12 +1,39 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { useColorScheme } from 'react-native';
-
-import { QueryClientProvider } from '@tanstack/react-query';
+import Toast, { BaseToast, BaseToastProps } from 'react-native-toast-message';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import queryClient from './src/api/queryClient';
 import RootNavigator from './src/navigations/root/RootNavigator';
-import Toast from 'react-native-toast-message';
+import { colors } from '@/constants';
+
+const toastConfig = {
+  success: (props: BaseToastProps) => (
+    <BaseToast
+      {...props}
+      style={{ borderLeftColor: colors.BLUE_500 }}
+      text1Style={{
+        fontSize: 14,
+      }}
+      text2Style={{
+        fontSize: 12,
+      }}
+    />
+  ),
+  error: (props: BaseToastProps) => (
+    <BaseToast
+      {...props}
+      style={{ borderLeftColor: colors.RED_500 }}
+      text1Style={{
+        fontSize: 14,
+      }}
+      text2Style={{
+        fontSize: 12,
+      }}
+    />
+  ),
+};
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -19,7 +46,7 @@ function App(): JSX.Element {
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
         <RootNavigator />
-        <Toast />
+        <Toast config={toastConfig} />
       </NavigationContainer>
     </QueryClientProvider>
   );
