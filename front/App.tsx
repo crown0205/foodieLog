cb7f92fc8ce906ba3e74503fc2730bca5e8a3485
crown0205 <crown0205@gmail.com>
2024-10-20 14:35:17 +0900

    chore : install toast message

--- front/App.tsx
+++ front/App.tsx
@@ -1,11 +1,12 @@
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import {useColorScheme} from 'react-native';
+import { useColorScheme } from 'react-native';
 
-import {QueryClientProvider} from '@tanstack/react-query';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import queryClient from './src/api/queryClient';
 import RootNavigator from './src/navigations/root/RootNavigator';
+import Toast from 'react-native-toast-message';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -18,6 +19,7 @@ function App(): JSX.Element {
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
         <RootNavigator />
+        <Toast />
       </NavigationContainer>
     </QueryClientProvider>
   );
