cf38d26ba7c9a3cd47decae6b1819e20b627fb24
crown0205 <crown0205@gmail.com>
2024-10-24 21:27:57 +0900

    feat : add kakao login with loading state

--- front/src/screens/auth/KakaoLoginScreen.tsx
+++ front/src/screens/auth/KakaoLoginScreen.tsx
@@ -1,21 +1,36 @@
 import { colors } from '@/constants';
 import useAuth from '@/hooks/queries/useAuth';
+import { deviceType } from '@/utils';
 import axios from 'axios';
-import React from 'react';
-import { Dimensions, SafeAreaView, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  ActivityIndicator,
+  Dimensions,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from 'react-native';
 import Config from 'react-native-config';
-import { WebView, WebViewMessageEvent } from 'react-native-webview';
+import {
+  WebView,
+  WebViewMessageEvent,
+  WebViewNavigation,
+} from 'react-native-webview';
 
-const REDIRECT_URL = `http://localhost:3030/auth/oauth/kakao`;
+const REDIRECT_URL = `${
+  deviceType === 'ios' ? 'http://localhost:3030/' : 'http://10.0.2.2:3030/'
+}auth/oauth/kakao`;
 const INJECTED_JAVASCRIPT = "window.ReactNativeWebView.postMessage('')";
 
 const KakaoLoginScreen = () => {
   const { kakaoLoginMutation } = useAuth();
+  const [isChangeNavigate, setIsChangeNavigate] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleOnMessage = (event: WebViewMessageEvent) => {
-    console.log('EVENT', event.nativeEvent.url);
     if (event.nativeEvent.url.includes(`${REDIRECT_URL}?code=`)) {
       const code = event.nativeEvent.url.replace(`${REDIRECT_URL}?code=`, '');
+
       requestToken(code);
     }
   };
@@ -35,15 +50,27 @@ const KakaoLoginScreen = () => {
     kakaoLoginMutation.mutate(response.data.access_token);
   };
 
+  const handleNavigationStateChange = (event: WebViewNavigation) => {
+    const isMatched = event.url.includes(`${REDIRECT_URL}?code=`);
+    setIsLoading(isMatched);
+    setIsChangeNavigate(event.loading);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
+      {(isLoading || isChangeNavigate) && (
+        <View style={styles.kakaoLadingContainer}>
+          <ActivityIndicator size="small" color={colors.BLUE_500} />
+        </View>
+      )}
       <WebView
         style={styles.container}
         source={{
           uri: `https://kauth.kakao.com/oauth/authorize?client_id=${Config.KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URL}&lang=ko&response_type=code`,
         }}
-        onMessage={handleOnMessage}
-        injectedJavaScript={INJECTED_JAVASCRIPT}
+        onMessage={handleOnMessage} // NOTE : 웹뷰에서 메시지를 받기 위한 이벤트
+        injectedJavaScript={INJECTED_JAVASCRIPT} // NOTE : 웹뷰에서 자바스크립트를 실행하기 위한 속성
+        onNavigationStateChange={handleNavigationStateChange} // NOTE : 로딩 상태를 확인하기 위한 이벤트
       />
     </SafeAreaView>
   );
