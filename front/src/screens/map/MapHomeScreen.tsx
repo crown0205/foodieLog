b72549a859c5323efeac7facad5145edd7559dbb
crown0205 <crown0205@gmail.com>
2024-11-03 02:45:58 +0900

    feat : add dark mode map style

--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -5,11 +5,13 @@ import useGetMarkers from '@/hooks/queries/useGetMarkers';
 import useModal from '@/hooks/useModal';
 import useMoveMapView from '@/hooks/useMoveMapView';
 import usePermission from '@/hooks/usePermission';
+import useThemeStorage from '@/hooks/useThemeStorage';
 import useUserLocation from '@/hooks/useUserLocation';
 import { MainDrawerParamList } from '@/navigations/drawer/MainDrawerNavigator';
 import { MapStackParamList } from '@/navigations/stack/MapStackNavigator';
 import useLocationStore from '@/store/useLocationStore';
-import mapStyle from '@/style/mapStyle';
+import getMapStyle from '@/style/mapStyle';
+import { ThemeMode } from '@/types';
 import { DrawerNavigationProp } from '@react-navigation/drawer';
 import {
   CompositeNavigationProp,
@@ -18,7 +20,6 @@ import {
 import { StackNavigationProp } from '@react-navigation/stack';
 import React, { useState } from 'react';
 import { Alert, Pressable, StyleSheet, View } from 'react-native';
-import Config from 'react-native-config';
 import MapView, {
   LatLng,
   LongPressEvent,
@@ -36,6 +37,8 @@ type Navigation = CompositeNavigationProp<
 >;
 
 const MapHomeScreen = () => {
+  const { theme } = useThemeStorage();
+  const styles = styling(theme);
   const inset = useSafeAreaInsets(); // NOTE : 상단의 상태바 높이를 가져옴
   const navigation = useNavigation<Navigation>();
 
@@ -96,7 +99,7 @@ const MapHomeScreen = () => {
         showsUserLocation // NOTE : 유저의 위치를 보여줌
         followsUserLocation // NOTE : 위치가 변경되면 따라가게
         showsMyLocationButton={false} // NOTE : 내 위치 버튼
-        customMapStyle={mapStyle}
+        customMapStyle={getMapStyle(theme)}
         onLongPress={handleLongPressMapView}
         onRegionChangeComplete={handleChangeDelta}
         region={{
@@ -119,17 +122,21 @@ const MapHomeScreen = () => {
         style={[styles.drawerButton, { top: inset.top || 20 }]}
         onPress={() => navigation.openDrawer()}
       >
-        <Ionicons name="menu" color={colors.BLACK} size={25} />
+        <Ionicons name="menu" color={colors[theme].BLACK} size={25} />
       </Pressable>
       <View style={styles.buttonList}>
         <Pressable style={styles.mapButton} onPress={handlePressAddPost}>
-          <MaterialIcons name="add" color={colors.BLACK} size={25} />
+          <MaterialIcons name="add" color={colors[theme].BLACK} size={25} />
         </Pressable>
         <Pressable style={styles.mapButton} onPress={handlePressSearchLocation}>
-          <Ionicons name="search" color={colors.BLACK} size={25} />
+          <Ionicons name="search" color={colors[theme].BLACK} size={25} />
         </Pressable>
         <Pressable style={styles.mapButton} onPress={handlePressUserLocation}>
-          <MaterialIcons name="my-location" color={colors.BLACK} size={25} />
+          <MaterialIcons
+            name="my-location"
+            color={colors[theme].BLACK}
+            size={25}
+          />
         </Pressable>
       </View>
 
@@ -142,47 +149,48 @@ const MapHomeScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  drawerButton: {
-    position: 'absolute',
-    left: 0,
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-    backgroundColor: colors.WHITE,
-    borderTopRightRadius: 50,
-    borderBottomRightRadius: 50,
-    shadowColor: colors.BLACK,
-    shadowOffset: {
-      width: 1,
-      height: 1,
+const styling = (theme: ThemeMode) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
     },
-    shadowOpacity: 0.5, // NOTE : 안드로이드에서는 적용되지 않음
-    elevation: 5, // NOTE : 안드로이드에서만 그림자 표현
-  },
-  buttonList: {
-    position: 'absolute',
-    bottom: 30,
-    right: 15,
-  },
-  mapButton: {
-    backgroundColor: colors.WHITE,
-    marginVertical: 5,
-    width: 45,
-    height: 45,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 50,
-    shadowColor: colors.BLACK,
-    shadowOffset: {
-      width: 1,
-      height: 2,
+    drawerButton: {
+      position: 'absolute',
+      left: 0,
+      paddingVertical: 10,
+      paddingHorizontal: 12,
+      backgroundColor: colors[theme].WHITE,
+      borderTopRightRadius: 50,
+      borderBottomRightRadius: 50,
+      shadowColor: colors[theme].BLACK,
+      shadowOffset: {
+        width: 1,
+        height: 1,
+      },
+      shadowOpacity: 0.5, // NOTE : 안드로이드에서는 적용되지 않음
+      elevation: 5, // NOTE : 안드로이드에서만 그림자 표현
     },
-    shadowOpacity: 0.5, // NOTE : 안드로이드에서는 적용되지 않음
-    elevation: 2, // NOTE : 안드로이드
-  },
-});
+    buttonList: {
+      position: 'absolute',
+      bottom: 30,
+      right: 15,
+    },
+    mapButton: {
+      backgroundColor: colors[theme].WHITE,
+      marginVertical: 5,
+      width: 45,
+      height: 45,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 50,
+      shadowColor: colors[theme].BLACK,
+      shadowOffset: {
+        width: 1,
+        height: 2,
+      },
+      shadowOpacity: 0.5, // NOTE : 안드로이드에서는 적용되지 않음
+      elevation: 2, // NOTE : 안드로이드
+    },
+  });
 
 export default MapHomeScreen;
